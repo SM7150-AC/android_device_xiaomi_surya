rio-afriansyah/device-xiaomi-surya-richard

surya: props: Override volume step values
Change-Id: I3409dcec2fd1e233c1792dca5b18d70d393b430d

BoardConfig.mk
@@ -107,6 +107,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 

SM7150-AC/android_device_xiaomi_surya

BoardConfig.mk
@@ -107,6 +107,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 