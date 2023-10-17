2849229f0c83fa550f41659c6ab860a816731f88
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-10-17 20:56:14 -0300

    surya: define 2nd cpu variant as cortex-a55
    Change-Id: Ibced71b76a69f3e418cf4311b8c5a773082a924a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a76
+TARGET_2ND_CPU_VARIANT := cortex-a55
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := surya,karna
